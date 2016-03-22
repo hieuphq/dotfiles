5ab68844b366a21f5bfe0478f4f87a7443d534c4
Huy Giang <gnhuy91@gmail.com>
2016-03-22 13:25:20 +0700

    added completion (from default Ubuntu .bashrc) & Heroku env

--- source/50_misc.sh
+++ source/50_misc.sh
@@ -10,6 +10,17 @@ export GREP_OPTIONS='--color=auto'
 # Prevent less from clearing the screen while still showing colors.
 export LESS=-XR
 
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
 # Set the terminal's title bar.
 function titlebar() {
   echo -n $'\e]0;'"$*"$'\a'
@@ -22,3 +33,6 @@ fi
 
 # Disable ansible cows }:]
 export ANSIBLE_NOCOWS=1
+
+# Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
