eb045634cdc5f5d79d0c2d04e5c69943a888b956
Huy Giang <gnhuy91@gmail.com>
2016-03-22 16:27:02 +0700

    added golang and gvm installation

---
+++ init/50_golang.sh
@@ -0,0 +1,13 @@
+# Define Golang version
+VERSION="1.5.3"
+ARCH="amd64"
+is_ubuntu && OS="linux"
+is_osx && OS="darwin"
+
+# Download Golang
+echo "Download Golang $VERSION.$OS-$ARCH"
+curl https://storage.googleapis.com/golang/go$VERSION.$OS-$ARCH.tar.gz | tar -C /usr/local -xz
+unset -v VERSION OS ARCH
+
+# loads Golang environment
+source $DOTFILES/source/50_golang.sh
