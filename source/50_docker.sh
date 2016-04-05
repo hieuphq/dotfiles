9b69635e131776a1e2cd6c947e459f1d0d82bd45
Huy Giang <gnhuy91@gmail.com>
2016-04-05 14:01:55 +0700

    added docker-compose alias & some possible aliases (not activated)

---
+++ source/50_docker.sh
@@ -0,0 +1,11 @@
+# ref: http://stackoverflow.com/questions/17236796/how-to-remove-old-docker-containers
+# remove old containers
+# docker ps -a | grep 'weeks ago' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+
+# remove all containers in an elegant way
+# docker rm `docker ps --no-trunc -aq`
+
+# remove all untagged images
+# docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi
+
+alias dkc="docker-compose"
