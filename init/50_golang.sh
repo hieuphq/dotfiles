ef9c65656627b306e76aa5399f429e9b34782b66
Huy Giang <gnhuy91@gmail.com>
2016-05-18 16:18:59 +0700

    use GO_VERSION instead of VERSION

--- init/50_golang.sh
+++ init/50_golang.sh
@@ -1,13 +1,13 @@
 # Define Golang version
-VERSION="1.6.2"
+GO_VERSION="1.6.2"
 ARCH="amd64"
 is_ubuntu && OS="linux"
 is_osx && OS="darwin"
 
 # Download Golang
-echo "Download Golang $VERSION.$OS-$ARCH"
-curl -s https://storage.googleapis.com/golang/go$VERSION.$OS-$ARCH.tar.gz | tar -C /usr/local -xz
-unset -v VERSION OS ARCH
+echo "Download Golang $GO_VERSION.$OS-$ARCH"
+curl -s https://storage.googleapis.com/golang/go$GO_VERSION.$OS-$ARCH.tar.gz | tar -C /usr/local -xz
+unset -v GO_VERSION OS ARCH
 
 # loads Golang environment
 source $DOTFILES/source/50_golang.sh
