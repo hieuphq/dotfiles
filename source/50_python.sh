8b672c248e557d7df20889869d3a012c911e1eff
Huy Giang <gnhuy91@gmail.com>
2016-05-19 23:41:52 +0700

    commented 'install packages without sudo' block also fixed 'no command `virtualenv-init`'

--- source/50_python.sh
+++ source/50_python.sh
@@ -1,22 +1,23 @@
-# Python & Ruby packages install without sudo
-pip() {
-  pip_exec=`which pip`
-  # also make sure venv is NOT activated
-  if [ -z "$VIRTUAL_ENV" ] && [ "$1" = "install" -o "$1" = "bundle" ]; then
-    cmd="$1"
-    shift
-    $pip_exec $cmd --user "$@"
-    # echo "--user"
-  else
-    $pip_exec "$@"
-  fi
-  # echo $pip_exec
-}
+# # Python & Ruby packages install without sudo
+# pip() {
+#   pip_exec=`which pip`
+#   # also make sure venv is NOT activated
+#   if [ -z "$VIRTUAL_ENV" ] && [ "$1" = "install" -o "$1" = "bundle" ]; then
+#     cmd="$1"
+#     shift
+#     $pip_exec $cmd --user "$@"
+#     # echo "--user"
+#   else
+#     $pip_exec "$@"
+#   fi
+#   # echo $pip_exec
+# }
 
 # pyenv
-export PATH="$HOME/.pyenv/bin:$PATH"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
+# eval "$(pyenv virtualenv-init -)"
 
 # remove all pip packages
 # http://stackoverflow.com/questions/11248073/what-is-the-easiest-way-to-remove-all-packages-installed-by-pip
