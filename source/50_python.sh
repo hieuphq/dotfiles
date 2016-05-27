674ceddf085c5657dc1fc3731177efed7a5854c4
Huy Giang <gnhuy91@gmail.com>
2016-05-27 11:35:46 +0700

    [python] moved init command to /init dir

--- source/50_python.sh
+++ source/50_python.sh
@@ -16,8 +16,6 @@
 # pyenv
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-# eval "$(pyenv virtualenv-init -)"
 
 # remove all pip packages
 # http://stackoverflow.com/questions/11248073/what-is-the-easiest-way-to-remove-all-packages-installed-by-pip
