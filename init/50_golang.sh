d7b819437de4398c4091ad6379e6cbb572a85852
Huy Giang <gnhuy91@gmail.com>
2016-08-05 13:24:38 +0700

    [go] bump Go verison to 1.6.3

--- init/50_golang.sh
+++ init/50_golang.sh
@@ -1,13 +1,19 @@
 # Define Golang version
-GO_VERSION="1.6.2"
+GO_VERSION="1.6.3"
 ARCH="amd64"
 is_ubuntu && OS="linux"
 is_osx && OS="darwin"
 
+# echo "Clean up previously installed Go version"
+# sudo rm -rf /usr/local/go/*
+
+GO_URL="https://storage.googleapis.com/golang/go$GO_VERSION.$OS-$ARCH.tar.gz"
+
 # Download Golang
-echo "Download Golang $GO_VERSION.$OS-$ARCH"
-sudo bash -c "curl -s https://storage.googleapis.com/golang/go$GO_VERSION.$OS-$ARCH.tar.gz | tar -C /usr/local -xz"
-unset -v GO_VERSION OS ARCH
+echo "Install Go $GO_VERSION.$OS-$ARCH -- $GO_URL"
+sudo bash -c "curl -s $GO_URL | tar -C /usr/local -xz"
+
+unset -v GO_VERSION GO_URL OS ARCH
 
 # loads Golang environment
 source $DOTFILES/source/50_golang.sh
