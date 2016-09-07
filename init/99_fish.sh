8f64f1344ee3cd5aedeae09ca84f22ac7d571482
Huy Giang <gnhuy91@gmail.com>
2016-09-07 17:04:55 +0700

    [fish] add gh tools

--- init/99_fish.sh
+++ init/99_fish.sh
@@ -1,6 +1,12 @@
 # install fish shell
 is_ubuntu && sudo apt-get install fish
 
+for v in "gh" "gl" "bb"
+do
+  ln -sf $DOTFILES/vendor/gh/functions/$v.fish $DOTFILES/link/.config/fish/functions/$v.fish
+  ln -sf $DOTFILES/vendor/gh/completions/$v.fish $DOTFILES/link/.config/fish/completions/$v.fish
+done
+
 # install https://github.com/oh-my-fish/oh-my-fish
 command -v fish >/dev/null 2>&1 && {
   sh -c "curl -L http://get.oh-my.fish | fish"
