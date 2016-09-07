a07b4b7484ec202c09c4e16b185c5bb0b7ce343f
Huy Giang <gnhuy91@gmail.com>
2016-09-07 17:05:23 +0700

    [gh] source files in vendor/

--- source/50_gh.sh
+++ source/50_gh.sh
@@ -1,10 +1,2 @@
-# https://github.com/dickeyxxx/gh
-GH_DIR="$HOME/src/github.com/dickeyxxx/gh"
-
-# clone gh if not already cloned
-if [[ ! -d "$GH_DIR" ]]; then
-    git clone https://github.com/dickeyxxx/gh.git $GH_DIR
-fi
-
-source $GH_DIR/bash/gh.bash
-source $GH_DIR/completions/gh.bash
+source $DOTFILES/vendor/gh/bash/gh.bash
+source $DOTFILES/vendor/gh/completions/gh.bash
