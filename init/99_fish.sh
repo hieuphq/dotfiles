4e32632c19982e004d47fbfd397588b299779bf5
Huy Giang <gnhuy91@gmail.com>
2016-09-07 17:26:39 +0700

    [fish] updated install gh steps

--- init/99_fish.sh
+++ init/99_fish.sh
@@ -1,11 +1,14 @@
 # install fish shell
 is_ubuntu && sudo apt-get install fish
 
+# install gh
+mkdir -p $DOTFILES/link/.config/fish/functions
+mkdir -p $DOTFILES/link/.config/fish/completions
 for v in "gh" "gl" "bb"
 do
   ln -sf $DOTFILES/vendor/gh/functions/$v.fish $DOTFILES/link/.config/fish/functions/$v.fish
-  ln -sf $DOTFILES/vendor/gh/completions/$v.fish $DOTFILES/link/.config/fish/completions/$v.fish
 done
+ln -sf $DOTFILES/vendor/gh/completions/gh.fish $DOTFILES/link/.config/fish/completions/gh.fish
 
 # install https://github.com/oh-my-fish/oh-my-fish
 command -v fish >/dev/null 2>&1 && {
