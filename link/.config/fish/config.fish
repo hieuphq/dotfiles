377078173259f1cea6283c15bdf0ddfa0999b559
Huy Giang <gnhuy91@gmail.com>
2016-09-08 12:48:47 +0700

    [fish] update config

--- link/.config/fish/config.fish
+++ link/.config/fish/config.fish
@@ -4,3 +4,5 @@ alias ll "ls -lhF"
 
 set -x GOPATH $HOME
 set PATH /usr/local/opt/go/libexec/bin $HOME/bin /usr/local/bin $PATH
+
+test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
