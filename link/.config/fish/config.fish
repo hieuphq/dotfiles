f9ba4a0a48f490d22e8fbb9a84bdd95204d3109a
Huy Giang <gnhuy91@gmail.com>
2016-09-08 14:25:36 +0700

    update fish config

--- link/.config/fish/config.fish
+++ link/.config/fish/config.fish
@@ -2,7 +2,9 @@ set fish_complete_path /usr/local/share/fish/completions $fish_complete_path
 
 alias ll "ls -lhF"
 
+set -x DOTFILES $HOME/.dotfiles
 set -x GOPATH $HOME
+
 set PATH /usr/local/opt/go/libexec/bin $HOME/bin /usr/local/bin $PATH
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
