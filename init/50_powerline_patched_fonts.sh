530563757ed8ffd69c868c8e4b5656430f9c7272
Huy Giang <gnhuy91@gmail.com>
2016-09-11 16:37:40 +0700

    [osx] add nerd font (Droid Sans Mono)

--- init/50_powerline_patched_fonts.sh
+++
@@ -1,9 +0,0 @@
-# patched fonts for compatible with powerline & fish themes
-# https://github.com/powerline/fonts
-POWERLINE_PATCHED_FONTS=$HOME/src/github.com/powerline/fonts
-
-mkdir -p $POWERLINE_PATCHED_FONTS
-git clone https://github.com/powerline/fonts $POWERLINE_PATCHED_FONTS
-
-# install patched fonts
-$POWERLINE_PATCHED_FONTS/install.sh
