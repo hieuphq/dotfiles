935c54495b5d74ad82c962b36f73796095345edc
Huy Giang <gnhuy91@gmail.com>
2017-01-23 23:46:07 +0700

    add more go tools

--- init/50_golang.sh
+++ init/50_golang.sh
@@ -21,6 +21,7 @@ source $DOTFILES/source/50_golang.sh
 go get -u -v golang.org/x/tools/cmd/goimports
 go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
+go get -u -v github.com/zmb3/gogetdoc
 go get -u -v github.com/golang/lint/golint
 go get -u -v github.com/lukehoban/go-outline
 go get -u -v sourcegraph.com/sqs/goreturns
@@ -28,3 +29,4 @@ go get -u -v golang.org/x/tools/cmd/gorename
 go get -u -v github.com/tpng/gopkgs
 go get -u -v github.com/newhook/go-symbols
 go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v github.com/cweill/gotests/...
