a4fb717b139dcb9f55a663fa7b468e68d0bff2d9
Huy Giang <gnhuy91@gmail.com>
2017-01-23 23:26:20 +0700

    make zsh default shell

--- init/30_osx_homebrew_recipes.sh
+++ init/30_osx_homebrew_recipes.sh
@@ -61,8 +61,14 @@ if [[ "$(type -P $binroot/bash)" && "$(cat /etc/shells | grep -q "$binroot/bash"
   e_header "Adding $binroot/bash to the list of acceptable shells"
   echo "$binroot/bash" | sudo tee -a /etc/shells >/dev/null
 fi
-if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/bash" ]]; then
-  e_header "Making $binroot/bash your default shell"
-  sudo chsh -s "$binroot/bash" "$USER" >/dev/null 2>&1
+
+# zsh
+if [[ "$(type -P $binroot/zsh)" && "$(cat /etc/shells | grep -q "$binroot/zsh")" ]]; then
+  e_header "Adding $binroot/zsh to the list of acceptable shells"
+  echo "$binroot/zsh" | sudo tee -a /etc/shells >/dev/null
+fi
+if [[ "$(dscl . -read ~ UserShell | awk '{print $2}')" != "$binroot/zsh" ]]; then
+  e_header "Making $binroot/zsh your default shell"
+  sudo chsh -s "$binroot/zsh" "$USER" >/dev/null 2>&1
   e_arrow "Please exit and restart all your shells."
 fi
